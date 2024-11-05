1c75949949b0ef0b2d6d160c54a43deaba8f39b8
Bhanu-goel <bhanugoel925@gmail.com>
2024-11-05 19:53:56 +0530

    test

--- backend/secret.py
+++ backend/secret.py
@@ -2,4 +2,4 @@ import secrets
 
 # Generate a random 256-bit key (32 bytes)
 secret_key = secrets.token_hex(32)
-print("Generated JWT Secret Key:", secret_key)
+print("Generated JWT Secret Key for token:", secret_key)
